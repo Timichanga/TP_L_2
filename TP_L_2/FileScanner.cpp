49b68e7bced742274af5db5b9321766b06980df2
Timichanga <153887369+Timichanga@users.noreply.github.com>
2023-12-20 13:19:49 +0300

    fix

--- TP_L_2/FileScanner.cpp
+++ TP_L_2/FileScanner.cpp
@@ -1,26 +1,45 @@
 #include "lib.h"
-FileScanner::FileScanner() { cout << "---Constructor FileScanner was called---" << endl; }
-FileScanner::~FileScanner() { cout << "---Destructor FileScanner was called---" << endl; }
-void FileScanner::scan_text()
-{
-	try
-	{
-		ifstream inp("data.txt");
-		while (inp.peek() != EOF) 
-		{
+FileScanner::FileScanner() { //cout << "Конструктор был вызван" << endl; 
+}
+FileScanner::~FileScanner() { //cout << "Деструктор был вызван" << endl; 
+}
+bool FileScanner::finder(const string& str, char ch) {
+	for (char c : str) {
+		if (c == ch) {
+			return true;
+		}
+	}
+	return false;
+}
+void FileScanner::scan_text() {
+	try {
+		ifstream inp("input.txt");
+		while (inp.peek() != EOF) {
 			string first;
 			string second;
 			inp >> first >> second;
-			if (!first.empty() && !second.empty()) 
-			{
-				cout << second << ' ' << first << ' ';
+			if (!first.empty() && !second.empty()) {
+				int flag = false;
+				for (char ch : {' ', '.', ',', '\n'}) {
+					if (finder(first, ch) || finder(second, ch)) {
+						flag = true;
+						break;
+					}
+				}
+				if (flag) {
+					cout << first << ' ' << second << ' ';
+				}
+				else {
+					cout << second << ' ' << first << ' ';
+				}
 			}
-			else 
-			{
+			else {
 				cout << first;
 			}
 		}
 	}
-	catch (const char* ex) { cout << "Error: " << ex << endl; }
+	catch (const char* ex) {
+		cout << "Ошибка: " << ex << endl;
+	}
 	cout << endl;
 }
